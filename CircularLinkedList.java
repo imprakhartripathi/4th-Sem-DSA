36b2b7efd1c94aec595b8b8a7d2ab9e328739b03
Prakhar Tripathi <prakhar.tripathi_cs22@gla.ac.in>
2024-01-31 22:10:42 +0530

    Final Commit 31st Jan

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -1,5 +1,6 @@
 public class CircularLinkedList {
     Node head;
+
     class Node {
         int data;
         Node next;
@@ -10,13 +11,114 @@ public class CircularLinkedList {
         }
     }
 
-    void AddLast(int nodedata) {
+    void AddFirst(int nodedata) {
         Node newnode = new Node(nodedata);
-        if(head==null){
+        if (head == null) {
+            head = newnode;
+            head.next = head;
+            return;
+        }
+        if (head.next == head) {
+            newnode.next = head;
+            head.next = newnode;
             head = newnode;
             return;
         }
-        newnode.next=head;
-        head=newnode;
+        Node last = head;
+        while (last.next != head) {
+            last = last.next;
+
+        }
+        newnode.next = head;
+        last.next = newnode;
+        head = newnode;
+
+    }
+
+    void AddLast(int nodedata) {
+        Node newnode = new Node(nodedata);
+        if (head == null) {
+            head = newnode;
+            return;
+        }
+        if (head.next == head) {
+            newnode.next = head;
+            head.next = newnode;
+            head = newnode;
+            return;
+        }
+        Node last = head;
+        while (last.next != head) {
+            last = last.next;
+        }
+
+        last.next = newnode;
+        newnode.next = head;
+
+    }
+
+    void DelFirst() {
+        if (head == null) {
+            System.out.println("List's empty bro what 'First Element' you tryna delete");
+            return;
+        }
+        if (head.next == head) {
+            head = null;
+            return;
+        }
+        Node last = head;
+        while (last.next != head) {
+            last = last.next;
+        }
+
+        head = head.next;
+        last.next = head;
+
+    }
+
+    void DelLast() {
+        if (head == null) {
+            System.out.println("List's empty bro what 'Last Element' you tryna delete");
+            return;
+        }
+        if (head.next == head) {
+            head = null;
+            return;
+        }
+        Node last = head;
+        Node prev = head;
+        while (last.next != head) {
+            prev = last;
+            last = last.next;
+        }
+
+        prev.next = head;
+    }
+
+    void PrintList() {
+        if (head == null) {
+            System.out.println("List's empty bro what you tryna print");
+            return;
+        }
+        Node last = head;
+        while (last.next != head) {
+            System.out.print(last.data + " ");
+            last = last.next;
+        }
+        System.out.print(last.data + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        CircularLinkedList list = new CircularLinkedList();
+        list.AddFirst(1);
+        list.AddLast(2);
+        list.AddLast(3);
+        list.AddLast(4);
+        list.AddFirst(0);
+        list.PrintList();
+        list.DelFirst();
+        list.DelLast();
+        list.PrintList();
     }
 }
