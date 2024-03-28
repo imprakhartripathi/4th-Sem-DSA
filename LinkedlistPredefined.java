024e47a3b02f690f06044221909790eaef5b3720
Prakhar Tripathi <prakhar.tripathi_cs22@gla.ac.in>
2024-03-28 09:53:23 +0530

    28th March

--- LinkedlistPredefined.java
+++ LinkedlistPredefined.java
@@ -2,11 +2,15 @@ import java.util.LinkedList;
 
 public class LinkedlistPredefined {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList<>();
-        list.addFirst(1);
-        list.addFirst(0);
-        list.add(2);
-        list.addLast(3);
+        LinkedList<Character> list = new LinkedList<>();
+        list.addFirst('1');
+        list.addFirst('0');
+        list.add('2');
+        list.addLast('3');
+        System.out.println(list);
         System.out.println(list.get(1));
+        for (int i=0; i<list.size(); i++){
+            System.out.print(list.get(i)+" ");
+        }
     }
 }
